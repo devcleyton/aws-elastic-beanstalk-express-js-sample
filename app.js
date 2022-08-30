592e4e2268bd155cea8fb1b3e355ad8767af3727
devcleyton <cleyton.oliveira.dev@gmail.com>
2022-08-30 17:42:04 -0300

    testando a primeira integração

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Primeira aplicação Aws'));
+app.get('/', (req, res) => res.send('Testando a primeira integração contínua.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
